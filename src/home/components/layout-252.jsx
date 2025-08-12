c8bebad15a6187b892fa8123b9f2b14313c6c926
Orhan Yorukoglu <orhany@inforouter.com>
2025-08-13 02:52:27 +0300

    Update layout-252.jsx

--- src/home/components/layout-252.jsx
+++ src/home/components/layout-252.jsx
@@ -78,7 +78,7 @@ export function Layout252() {
           <div className="w-full">
             <div className="mb-6 md:mb-8">
               <img
-                src="/images/makeup.jpeg"
+                src="/images/makeup1.jpeg"
                 alt="Creative influencer in purple neon lighting environment"
                 className="rounded-image"
               />
