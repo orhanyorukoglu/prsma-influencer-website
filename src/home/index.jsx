d5e1179813b9c670d9a076c14ca0ef735ea165c1
Orhan Yorukoglu <orhany@inforouter.com>
2025-08-13 03:41:47 +0300

    Update index.jsx

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -7,7 +7,6 @@ import { Layout4 } from "./components/layout-04";
 import { Testimonial5 } from "./components/testimonial-05";
 import { Cta1 } from "./components/cta-01";
 import { Team1 } from "./components/team-01";
-import { Pain }  from "./components/pain-points.jsx";
 import { Contact1 } from "./components/contact-01";
 import { Pricing25 } from "./components/pricing-25";
 
@@ -24,7 +23,6 @@ export default function Home() {
       <Cta1 />
       <Team1 />
       <Contact1 />
-      <Pain />
       <Pricing25 />
     </div>
   );
